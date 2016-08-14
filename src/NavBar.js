4fa20c463423a2ae6ef40aa19f1d906fc0025b6c
Mark Thompson <mthommo79@gmail.com>
2016-08-14 21:26:53 +1000

    Added PropTypes for navBarStyle

--- src/NavBar.js
+++ src/NavBar.js
@@ -162,7 +162,7 @@ const propTypes = {
   titleOpacity: PropTypes.number,
   titleProps: PropTypes.any,
   position: PropTypes.object,
-  navigationBarStyle: View.propTypes.style,
+  navigationBarStyle: View.propTypes.style || PropTypes.arrayOf(PropTypes.string),
   renderTitle: PropTypes.any,
 };
 
